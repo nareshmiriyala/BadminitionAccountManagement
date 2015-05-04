a2209507ee5e8f1e2b7e32bec1224b76422128a6
Naresh Chandra Miriyala <ncmiriyala@gmail.com>
2015-05-04 22:52:31 +1000

    adding login page

--- web/src/main/java/com/dellnaresh/controller/RegisterServlet.java
+++ web/src/main/java/com/dellnaresh/controller/RegisterServlet.java
@@ -3,6 +3,7 @@ package com.dellnaresh.controller;
 import com.dellnaresh.common.remote.RemotePlayerService;
 import com.dellnaresh.common.remote.entities.Player;
 import org.json.JSONObject;
+import org.json.simple.parser.JSONParser;
 
 import javax.ejb.EJB;
 import javax.servlet.ServletException;
@@ -15,44 +16,40 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.Timestamp;
 import java.util.Date;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
 
 /**
  * Created by nareshm on 2/05/2015.
  */
-@WebServlet(name = "RegisterServlet",urlPatterns = {"/register"})
+@WebServlet(name = "RegisterServlet", urlPatterns = {"/register"})
 public class RegisterServlet extends HttpServlet {
 
-   @EJB
+    @EJB
     RemotePlayerService remotePlayerService;
+
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         try {
             StringBuffer sb = new StringBuffer();
-            try
-            {
+            try {
                 BufferedReader reader = request.getReader();
                 String line = null;
-                while ((line = reader.readLine()) != null)
-                {
+                while ((line = reader.readLine()) != null) {
                     sb.append(line);
                 }
-            } catch (Exception e) { e.printStackTrace(); }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
             JSONParser parser = new JSONParser();
             JSONObject joPlayer = null;
-//            try
-//            {
-                joPlayer = new JSONObject(sb.toString());
-//            } catch (ParseException e) { e.printStackTrace(); }
+            joPlayer = new JSONObject(sb.toString());
 
             String firstname = (String) joPlayer.get("firstname");
             String lastName = (String) joPlayer.get("lastname");
             String password = (String) joPlayer.get("password");
             String email = (String) joPlayer.get("email");
-            Integer contactno=(Integer)joPlayer.get("contactno");
-            String address=(String)joPlayer.get("address");
+            Integer contactno = (Integer) joPlayer.get("contactno");
+            String address = (String) joPlayer.get("address");
 
-            callCreatePlayer(firstname,lastName,email,contactno,address,password);
+            callCreatePlayer(firstname, lastName, email, contactno, address, password);
             response.setContentType("text/html");
             PrintWriter out = response.getWriter();
             out.write("A new user " + firstname + " has been created.");
@@ -64,7 +61,7 @@ public class RegisterServlet extends HttpServlet {
         }
     }
 
-    private void callCreatePlayer(String firstName ,String lastName,String email,long conactno,String address,String password) throws Exception {
+    private void callCreatePlayer(String firstName, String lastName, String email, long conactno, String address, String password) throws Exception {
         Player player = new Player();
         player.setActive((byte) 1);
         player.setAddress(address);
@@ -78,6 +75,6 @@ public class RegisterServlet extends HttpServlet {
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-    doPost(request,response);
+        doPost(request, response);
     }
 }
