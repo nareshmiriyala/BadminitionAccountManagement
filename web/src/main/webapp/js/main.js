c63431577ad3de8f47ce4b5f907bb60345637594
Naresh Chandra Miriyala <ncmiriyala@gmail.com>
2015-09-13 20:08:02 +1000

    commit js and css files

--- web/src/main/webapp/js/main.js
+++ web/src/main/webapp/js/main.js
@@ -1,55 +1,68 @@
 /**
- * AngularJS Tutorial 1
- * @author Nick Kaye <nick.c.kaye@gmail.com>
- */
-
-/**
  * Main AngularJS Web Application
  */
 var app = angular.module('badmintonWebApp', [
-  'ngRoute'
+    'ngRoute'
 ]);
 
 /**
  * Configure the Routes
  */
 app.config(['$routeProvider', function ($routeProvider) {
-  $routeProvider
-    // Home
-    .when("/", {templateUrl: "pages/home.html", controller: "PageCtrl"})
-    // Pages
-    .when("/about", {templateUrl: "pages/about.html", controller: "PageCtrl"})
-    .when("/faq", {templateUrl: "pages/faq.html", controller: "PageCtrl"})
-    .when("/pricing", {templateUrl: "pages/pricing.html", controller: "PageCtrl"})
-    .when("/services", {templateUrl: "pages/services.html", controller: "PageCtrl"})
-    .when("/contact", {templateUrl: "pages/contact.html", controller: "PageCtrl"})
-    // Blog
-    .when("/blog", {templateUrl: "pages/blog.html", controller: "BlogCtrl"})
-    .when("/blog/post", {templateUrl: "pages/blog_item.html", controller: "BlogCtrl"})
-    // else 404
-    .otherwise("/404", {templateUrl: "pages/404.html", controller: "PageCtrl"});
-}]);
+        $routeProvider
+                // Home
+                .when("/",
+                        {
+                            templateUrl: "pages/home.html",
+                            controller: "PageCtrl"
+                        })
+                // Pages
+                .when("/about",
+                        {
+                            templateUrl: "pages/about.html",
+                            controller: "PageCtrl"
+                        })
+                .when("/faq",
+                        {
+                            templateUrl: "pages/faq.html",
+                            controller: "PageCtrl"
+                        })
+                .when("/pricing",
+                        {
+                            templateUrl: "pages/pricing.html",
+                            controller: "PageCtrl"
+                        })
+                .when("/services",
+                        {
+                            templateUrl: "pages/services.html",
+                            controller: "PageCtrl"
+                        })
+                .when("/contact",
+                        {
+                            templateUrl: "pages/contact.html",
+                            controller: "PageCtrl"
+                        })
+                // Blog
+                .when("/blog",
+                        {
+                            templateUrl: "pages/blog.html",
+                            controller: "BlogCtrl"
+                        })
+                .when("/blog/post",
+                        {
+                            templateUrl: "pages/blog_item.html",
+                            controller: "BlogCtrl"
+                        })
+                .when("/login",
+                        {
+                            templateUrl: "pages/login.html",
+                            controller: "PageCtrl"
+                        })
+                // else 404
+                .otherwise("/404",
+                        {
+                            templateUrl: "pages/404.html",
+                            controller: "PageCtrl"
+                        });
+    }]);
 
-/**
- * Controls the Blog
- */
-app.controller('BlogCtrl', function (/* $scope, $location, $http */) {
-  console.log("Blog Controller reporting for duty.");
-});
-
-/**
- * Controls all other Pages
- */
-app.controller('PageCtrl', function (/* $scope, $location, $http */) {
-  console.log("Page Controller reporting for duty.");
-
-  // Activates the Carousel
-  $('.carousel').carousel({
-    interval: 5000
-  });
-
-  // Activates Tooltips for Social Links
-  $('.tooltip-social').tooltip({
-    selector: "a[data-toggle=tooltip]"
-  })
-});
--- web/src/test/java/com/dellnaresh/controller/jaxrs/CourtHireServiceTest.java
+++ web/src/test/java/com/dellnaresh/controller/jaxrs/CourtHireServiceTest.java
@@ -1,87 +1,87 @@
-package com.dellnaresh.controller.jaxrs;
-
-import com.dellnaresh.common.remote.RemotePlayerService;
-import com.dellnaresh.common.remote.entities.BadmintonHire;
-import com.dellnaresh.dao.DefaultPlayerDAO;
-import com.dellnaresh.ejb.BadmintonPlayerService;
-import com.dellnaresh.interfaces.PlayerDAO;
-import org.jboss.arquillian.container.test.api.Deployment;
-import org.jboss.arquillian.container.test.api.RunAsClient;
-import org.jboss.arquillian.junit.Arquillian;
-import org.jboss.arquillian.test.api.ArquillianResource;
-import org.jboss.shrinkwrap.api.Archive;
-import org.jboss.shrinkwrap.api.ShrinkWrap;
-import org.jboss.shrinkwrap.api.asset.EmptyAsset;
-import org.jboss.shrinkwrap.api.spec.WebArchive;
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import javax.ws.rs.client.Client;
-import javax.ws.rs.client.ClientBuilder;
-import javax.ws.rs.client.Entity;
-import javax.ws.rs.client.WebTarget;
-import java.net.MalformedURLException;
-import java.net.URI;
-import java.net.URL;
-
-import static org.junit.Assert.assertEquals;
-
-/**
- * Created by nareshm on 10/05/2015.
- */
-@RunWith(Arquillian.class)
-@RunAsClient
-public class CourtHireServiceTest {
-    private static WebTarget target;
-    Logger logger = LoggerFactory.getLogger(CourtHireServiceTest.class);
-
-@Deployment
-public static Archive<?> createDeployment() {
-    return ShrinkWrap.create(WebArchive.class, "test.war")
-            .addClasses(CourtHireService.class, BadmintonApp.class)
-            .addPackages(true, BadmintonHire.class.getPackage())
-            .addPackages(true, BadmintonPlayerService.class.getPackage(), PlayerDAO.class.getPackage(), DefaultPlayerDAO.class.getPackage(), RemotePlayerService.class.getPackage())
-            .addAsResource("test-persistence.xml", "META-INF/persistence.xml")
-            .addAsWebInfResource("jbossas-ds.xml")
-            .addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
-}
-    @ArquillianResource
-    private URL base;
-
-    @Before
-    public void setupClass() throws MalformedURLException {
-        Client client = ClientBuilder.newClient();
-        logger.debug("Base URL",base);
-        target = client.target(URI.create(new URL(base, "app/hirecourt").toExternalForm()));
-    }
-    /**
-     * Test of POST method, of class MyResource.
-     */
-    @Test
-    public void test1Post() {
-        String json="{\"courtName\":\"Endeavour Hills\",\n" +
-                "\"dateHired\": \"2015-05-10\",\n" +
-                "\"startTime\": \"2015-05-10T08:30:00\",\n" +
-                "\"endTime\":\"2015-05-10T10:30:00\",\n" +
-                "\"moneyPaid\":35.50,\n" +
-                "\"payer\":1}";
-        logger.info("base url"+base);
-        javax.ws.rs.core.Response post = target.request().post(Entity.json(json));
-         logger.info("Status",post.getStatus());
-        System.out.println(post.getStatus());
-//        assertEquals("Status",200,post.getStatus());
-
-    }
-    @Test
-    public void testGetJSON() throws Exception{
-
-        javax.ws.rs.core.Response response = target.request().get();
-        Object responseEntity = response.getEntity();
-        System.out.println(responseEntity);
-    }
-
-
-}
+//package com.dellnaresh.controller.jaxrs;
+//
+//import com.dellnaresh.common.remote.RemotePlayerService;
+//import com.dellnaresh.common.remote.entities.BadmintonHire;
+//import com.dellnaresh.dao.DefaultPlayerDAO;
+//import com.dellnaresh.ejb.BadmintonPlayerService;
+//import com.dellnaresh.interfaces.PlayerDAO;
+//import org.jboss.arquillian.container.test.api.Deployment;
+//import org.jboss.arquillian.container.test.api.RunAsClient;
+//import org.jboss.arquillian.junit.Arquillian;
+//import org.jboss.arquillian.test.api.ArquillianResource;
+//import org.jboss.shrinkwrap.api.Archive;
+//import org.jboss.shrinkwrap.api.ShrinkWrap;
+//import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+//import org.jboss.shrinkwrap.api.spec.WebArchive;
+//import org.junit.Before;
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+//
+//import javax.ws.rs.client.Client;
+//import javax.ws.rs.client.ClientBuilder;
+//import javax.ws.rs.client.Entity;
+//import javax.ws.rs.client.WebTarget;
+//import java.net.MalformedURLException;
+//import java.net.URI;
+//import java.net.URL;
+//
+//import static org.junit.Assert.assertEquals;
+//
+///**
+// * Created by nareshm on 10/05/2015.
+// */
+//@RunWith(Arquillian.class)
+//@RunAsClient
+//public class CourtHireServiceTest {
+//    private static WebTarget target;
+//    Logger logger = LoggerFactory.getLogger(CourtHireServiceTest.class);
+//
+//@Deployment
+//public static Archive<?> createDeployment() {
+//    return ShrinkWrap.create(WebArchive.class, "test.war")
+//            .addClasses(CourtHireService.class, BadmintonApp.class)
+//            .addPackages(true, BadmintonHire.class.getPackage())
+//            .addPackages(true, BadmintonPlayerService.class.getPackage(), PlayerDAO.class.getPackage(), DefaultPlayerDAO.class.getPackage(), RemotePlayerService.class.getPackage())
+//            .addAsResource("test-persistence.xml", "META-INF/persistence.xml")
+//            .addAsWebInfResource("jbossas-ds.xml")
+//            .addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
+//}
+//    @ArquillianResource
+//    private URL base;
+//
+//    @Before
+//    public void setupClass() throws MalformedURLException {
+//        Client client = ClientBuilder.newClient();
+//        logger.debug("Base URL",base);
+//        target = client.target(URI.create(new URL(base, "app/hirecourt").toExternalForm()));
+//    }
+//    /**
+//     * Test of POST method, of class MyResource.
+//     */
+//    @Test
+//    public void test1Post() {
+//        String json="{\"courtName\":\"Endeavour Hills\",\n" +
+//                "\"dateHired\": \"2015-05-10\",\n" +
+//                "\"startTime\": \"2015-05-10T08:30:00\",\n" +
+//                "\"endTime\":\"2015-05-10T10:30:00\",\n" +
+//                "\"moneyPaid\":35.50,\n" +
+//                "\"payer\":1}";
+//        logger.info("base url"+base);
+//        javax.ws.rs.core.Response post = target.request().post(Entity.json(json));
+//         logger.info("Status",post.getStatus());
+//        System.out.println(post.getStatus());
+////        assertEquals("Status",200,post.getStatus());
+//
+//    }
+//    @Test
+//    public void testGetJSON() throws Exception{
+//
+//        javax.ws.rs.core.Response response = target.request().get();
+//        Object responseEntity = response.getEntity();
+//        System.out.println(responseEntity);
+//    }
+//
+//
+//}
